b35b94fe6e1f2fd4da613ead645a2aa5fb5643d7
shunt <terawhiz@gmail.com>
2023-02-06 19:09:32 +0530

    DiceCTF - 2023

---
+++ DiceCTF-2023/misc/pike/server.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python
+
+import rpyc
+from rpyc.utils.server import ThreadedServer
+
+class CalcService(rpyc.Service):
+    def exposed_add(self, l, r):
+        print(type(l))
+        print(type(r))
+        return l + r
+    def exposed_sub(self, l, r):
+        return l - r
+    def exposed_mul(self, l, r):
+        return l * r
+    def exposed_div(self, l, r):
+        return l / r
+
+if __name__ == "__main__":
+    server = ThreadedServer(CalcService, port=9999)
+    server.start()
---
+++ README.md
@@ -0,0 +1,5 @@
+# CTF-Writeups
+
+Writeups for all the CTF challenges I've solved.
+Feel free to contact me for any reason.
+[Twitter](https://twitter.com/teraWhiz) | [Discord](https://discordapp.com/users/Shunt#4275)
